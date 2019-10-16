0a98439d58aff8391ef690857877c727e930c8cc
nakayashiki <tnakayashiki01@gmail.com>
2019-10-16 15:14:18 +0900

    add get_positions_from_sfen_without_startpos()

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ extern crate bitintr;
 extern crate numpy;
 extern crate pyo3;
 extern crate rand;
+extern crate rayon;
 
 pub mod bitboard;
 pub mod checkmate;
@@ -15,6 +16,20 @@ pub mod types;
 pub mod zobrist;
 
 use pyo3::prelude::*;
+use pyo3::wrap_pyfunction;
+use rayon::prelude::*;
+
+#[pyfunction]
+pub fn get_positions_from_sfen_without_startpos(sfen_kifs: std::vec::Vec<String>) -> std::vec::Vec<position::Position> {
+    let positions = sfen_kifs.par_iter().map(|x| {
+            let mut position = position::Position::empty_board();
+            position.set_sfen_without_startpos(x);
+
+            position
+        }).collect();
+
+    return positions;
+}
 
 #[pymodule]
 fn minishogilib(_py: Python, m: &PyModule) -> PyResult<()> {
@@ -27,6 +42,8 @@ fn minishogilib(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<position::Position>()?;
     m.add_class::<mcts::MCTS>()?;
     m.add_class::<r#move::Move>()?;
+
+    m.add_wrapped(wrap_pyfunction!(get_positions_from_sfen_without_startpos))?;
 
     Ok(())
 }
