b56d3b98fed3b6ae4b90deafe3364ad9f0d6cf3e
nakayashiki <tnakayashiki01@gmail.com>
2019-10-20 00:25:48 +0900

    [WIP] multi thread sampling

--- src/reservoir.rs
+++ src/reservoir.rs
@@ -1,6 +1,6 @@
 use std::collections::VecDeque;
-use std::io::{BufRead, BufReader};
 use std::fs::File;
+use std::io::{BufRead, BufReader};
 
 use numpy::PyArray1;
 use pyo3::prelude::*;
@@ -8,6 +8,8 @@ use rand::{distributions::Uniform, Rng};
 use rayon::prelude::*;
 use record::*;
 use position::*;
+
+use std::time::{Duration, Instant};
 
 #[pyclass]
 pub struct Reservoir {
@@ -53,16 +55,24 @@ impl Reservoir {
     }
 
     pub fn sample(&self, py: Python, mini_batch_size: usize) -> (Py<PyArray1<f32>>, Py<PyArray1<f32>>, Py<PyArray1<f32>>) {
+        let start = Instant::now();
+
         let mut cumulative_plys = vec![0; self.max_size + 1];
 
         for i in 0..self.max_size {
             cumulative_plys[i + 1] = cumulative_plys[i] + self.learning_targets[i].len();
         }
 
+        let end = start.elapsed();
+        println!("cumulative {}.{:03} sec", end.as_secs(), end.subsec_nanos() / 1000000);
+
         let range = Uniform::from(0..cumulative_plys[self.max_size]);
         let mut indicies: std::vec::Vec<usize> = rand::thread_rng().sample_iter(&range).take(mini_batch_size).collect();
 
         indicies.sort();
+
+        let end = start.elapsed();
+        println!("sort {}.{:03} sec", end.as_secs(), end.subsec_nanos() / 1000000);
 
         let mut targets = vec![(0, 0); mini_batch_size];
 
@@ -87,30 +97,31 @@ impl Reservoir {
             lo = ok;
         }
 
-        let mut ins = vec![0f32; mini_batch_size * (8 * 33 + 2) * 5 * 5];
-        let mut policies = vec![0f32; mini_batch_size * (69 * 5 * 5)];
-        let mut values = vec![0f32; mini_batch_size];
+        let end = start.elapsed();
+        println!("targets {}.{:03} sec", end.as_secs(), end.subsec_nanos() / 1000000);
 
-        for b in 0..mini_batch_size {
-            let index = targets[b].0;
-            let ply = targets[b].1;
+        let end = start.elapsed();
+        println!("allocate {}.{:03} sec", end.as_secs(), end.subsec_nanos() / 1000000);
 
-            // Input.
+        let data: std::vec::Vec<_> = targets.par_iter().map(move |&target| {
+            let index = target.0;
+            let ply = target.1;
+
             let mut position = Position::empty_board();
             position.set_start_position();
-            let sfen_moves = &self.records[index].sfen_kif[..ply];
-            for m in sfen_moves {
+
+            for (i, m) in self.records[index].sfen_kif.iter().enumerate() {
+                if i == ply {
+                    break;
+                }
+
                 let m = position.sfen_to_move(m);
                 position.do_move(&m);
             }
 
             let nninput = position.to_alphazero_input_array();
 
-            {
-                let (left, _right) = ins[(b * (8 * 33 + 2) * 5 * 5)..].split_at_mut((8 * 33 + 2) * 5 * 5);
-                left.clone_from_slice(&nninput);
-            }
-
+            let mut policy = vec![0f32; 69 * 5 * 5];
             // Policy.
             let (sum_n, _q, playouts) = &self.records[index].mcts_result[ply];
 
@@ -118,18 +129,44 @@ impl Reservoir {
                 let m = position.sfen_to_move(&playout.0);
                 let n = playout.1;
 
-                policies[b * (69 * 5 * 5) + m.to_policy_index()] = n as f32 / *sum_n as f32;
+                policy[m.to_policy_index()] = n as f32 / *sum_n as f32;
             }
 
             // Value.
-            values[b] = if self.records[index].winner == 2 {
+            let value = if self.records[index].winner == 2 {
                 0.0
             } else if self.records[index].winner == position.get_side_to_move() {
                 1.0
             } else {
                 -1.0
             };
+
+            (nninput, policy, value)
+        }).collect();
+
+        let end = start.elapsed();
+        println!("sample {}.{:03} sec", end.as_secs(), end.subsec_nanos() / 1000000);
+
+        let mut ins = vec![0f32; mini_batch_size * (8 * 33 + 2) * 5 * 5];
+        let mut policies = vec![0f32; mini_batch_size * (69 * 5 * 5)];
+        let mut values = vec![0f32; mini_batch_size];
+
+        for (i, batch) in data.iter().enumerate() {
+            /*
+            let (left, _right) = ins[(i * (8 * 33 + 2) * 5 * 5)..].split_at_mut((8 * 33 + 2) * 5 * 5);
+            left.clone_from_slice(&batch.0);
+
+            let (left, _right) = policies[(i * 69 * 5 * 5)..].split_at_mut(69 * 5 * 5);
+            left.clone_from_slice(&batch.1);
+            */
+
+            values[i] = batch.2;
         }
+
+        let end = start.elapsed();
+        println!("copy {}.{:03} sec", end.as_secs(), end.subsec_nanos() / 1000000);
+
+        let end = start.elapsed();
 
         (PyArray1::from_slice(py, &ins).to_owned(),
          PyArray1::from_slice(py, &policies).to_owned(),
