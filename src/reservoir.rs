ac174bee9137d93241c5236a0933cf3f9c521276
nakayashiki <tnakayashiki01@gmail.com>
2019-10-19 16:20:39 +0900

    blueprints

--- src/reservoir.rs
+++ src/reservoir.rs
@@ -3,6 +3,7 @@ use std::io::{BufRead, BufReader};
 use std::fs::File;
 
 use pyo3::prelude::*;
+use rand::{distributions::Uniform, Rng}; // 0.7.0
 use record::*;
 
 #[pyclass]
@@ -45,6 +46,42 @@ impl Reservoir {
 
         for line in file.lines().filter_map(|x| x.ok()) {
             self.push(&line);
+        }
+    }
+
+    pub fn sample(&self, mini_batch_size: usize) {
+        let mut cumulative_plys = vec![0; self.max_size + 1];
+
+        for i in 0..self.max_size {
+            cumulative_plys[i + 1] = cumulative_plys[i] + self.learning_targets[i].len();
+        }
+
+        let range = Uniform::from(0..cumulative_plys[self.max_size]);
+        let mut indicies: std::vec::Vec<usize> = rand::thread_rng().sample_iter(&range).take(mini_batch_size).collect();
+
+        indicies.sort();
+
+        let mut target_plys = vec![(0, 0); mini_batch_size];
+
+        let mut lo = 0;
+        for i in 0..mini_batch_size {
+            let mut ok = lo;
+            let mut ng = self.max_size + 1;
+
+            while ng - ok > 1 {
+                let mid = (ok + ng) / 2;
+
+                if indicies[i] >= cumulative_plys[ok] {
+                    ok = mid;
+                } else {
+                    ng = mid;
+                }
+            }
+
+            let ply = self.learning_targets[ok][indicies[i] - cumulative_plys[ok]];
+            target_plys[i] = (ok, ply);
+
+            lo = ok;
         }
     }
 }
