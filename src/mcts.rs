2050a568b1c40538483469aa07b2325778c0b700
nakayashiki <tnakayashiki01@gmail.com>
2019-12-11 17:36:12 +0900

    softmax sampling not from raw visit counts but from proportional to visit counts

--- src/mcts.rs
+++ src/mcts.rs
@@ -179,7 +179,7 @@ impl MCTS {
         let mut sum: f32 = 0.0;
 
         for child in &self.game_tree[node].children {
-            sum += ((self.game_tree[*child].n as i32 - visit_max) as f32 / temperature).exp();
+            sum += ((self.game_tree[*child].n as i32 / visit_max)  as f32 / temperature).exp();
         }
 
         let mut rng = rand::thread_rng();
@@ -188,13 +188,46 @@ impl MCTS {
         let mut cum: f32 = 0.0;
 
         for child in &self.game_tree[node].children {
-            cum += ((self.game_tree[*child].n as i32 - visit_max) as f32 / temperature).exp() / sum;
+            cum += ((self.game_tree[*child].n as i32 / visit_max) as f32 / temperature).exp() / sum;
             if r < cum {
                 return self.game_tree[*child].m;
             }
         }
 
         return self.game_tree[self.game_tree[node].children[0]].m;
+    }
+
+    pub fn softmax_sample_among_top_moves(&self, node: usize, away: f32, temperature: f32) -> Move {
+        let best_child: usize = self.select_n_max_child(node);
+        let visit_max: i32 = self.game_tree[best_child].n as i32;
+
+        let mut sum: f32 = 0.0;
+
+        for child in &self.game_tree[node].children {
+            if self.game_tree[*child].n < (visit_max as f32 * (1.0 - away)) as u32 {
+                continue;
+            }
+
+            sum += ((self.game_tree[*child].n as i32 / visit_max) as f32 / temperature).exp();
+        }
+
+        let mut rng = rand::thread_rng();
+        let r: f32 = rng.gen();
+
+        let mut cum: f32 = 0.0;
+
+        for child in &self.game_tree[node].children {
+            if self.game_tree[*child].n < (visit_max as f32 * (1.0 - away)) as u32 {
+                continue;
+            }
+
+            cum += ((self.game_tree[*child].n as i32 / visit_max) as f32 / temperature).exp() / sum;
+            if r < cum {
+                return self.game_tree[*child].m;
+            }
+        }
+
+        return self.game_tree[best_child].m;
     }
 
     pub fn print(&self, root: usize) {
